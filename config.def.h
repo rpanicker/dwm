cbbc0d6b81978c0cdd658e3c0c30b2de0fb0e4b9
rpanicker <rijesh.panicker@gmail.com>
2020-10-28 22:24:29 +0530

    Patched restart code and added keychord super+shift+r

--- config.def.h
+++ config.def.h
@@ -125,7 +125,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
+	{ MODKEY|ShiftMask, 		XK_r,      quit,           {1} }, 
 };
 
 /* button definitions */
